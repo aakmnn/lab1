022076568f37aa6ab2f7e267949f1b83b24b9aad
aakmnn <a_omargali@kbtu.kz>
2024-09-19 16:24:28 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,354 @@
+print ("Hello, word!")
+
+if 5 > 2:
+    print ("Five is greater than two!")
+
+
+
+    if 5 > 2:
+        print ("five is greather than two!")
+    if 5 > 2:
+        print ("five is greather than two!")
+
+
+x = 5
+y = "Hello, World!"
+
+print (x)
+print (y)
+
+
+#This is a comment.
+print ("Hello, World!")
+
+
+print ("Hello, world!")
+#This is a comment 
+
+
+#print ("Hello, World!")
+print ("Cheers, Mate!")
+
+#This is a comment 
+#written in
+#more than just one line
+print ("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+x = str(3)    
+y = int(3)    
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = "Python is awesome"
+print(x)
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+x = 5
+print(type(x))
+
+
+x = str("Hello World")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = int(20)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = float(20.5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = complex(1j)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = list(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = tuple(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = range(6)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = dict(name="John", age=36)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = set(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = frozenset(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#convert from int to float:
+x = float(1)
+#convert from float to int:
+y = int(2.8)
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import random
+print(random.randrange(1, 10))
+
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
